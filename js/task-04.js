7037d545c9b1fcf32b3228f65801e67f56128a46
Kinderpro <131799650+Kinderpro@users.noreply.github.com>
2023-08-29 22:43:37 +0300

    task4

--- js/task-04.js
+++ js/task-04.js
@@ -1,15 +1,15 @@
-const btnPlus = document.querySelector('button[data-action="decrement"]');
-const btnMinus = document.querySelector('button[data-action="increment"]');
+const btnMinus = document.querySelector('button[data-action="decrement"]');
+const btnPlus = document.querySelector('button[data-action="increment"]');
 const valueElement = document.getElementById("value");
 
 let counterValue = 0;
 
 btnMinus.addEventListener("click", function () {
-  counterValue += 1;
+  counterValue -= 1;
   valueElement.textContent = counterValue;
 });
 
 btnPlus.addEventListener("click", function () {
-  counterValue -= 1;
+  counterValue += 1;
   valueElement.textContent = counterValue;
 });
